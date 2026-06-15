b655508f38b0e3f4235c265e30a26eea8e9c9131
Identity Service Fix <fix@identity-service.local>
2026-06-15 16:17:51 +0700

    up

--- src/main/java/com/example/foodshop/identity/service/UserService.java
+++ src/main/java/com/example/foodshop/identity/service/UserService.java
@@ -35,7 +35,7 @@ public class UserService {
     @Value("${app.admin.default.password:admin123}")
     private String defaultAdminPassword;
 
-    @Value("${app.order.service.url:lb://order-service}")
+    @Value("${app.order.service.url:https://order-service.onrender.com}")
     private String orderServiceUrl;
 
     @Autowired
